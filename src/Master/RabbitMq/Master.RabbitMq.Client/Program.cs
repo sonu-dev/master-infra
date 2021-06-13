f3593de5ed69672c9d51ade27de527fcfb32ce76
Sonu Thakur <sonu.concious@gmail.com>
2021-06-13 23:40:49 +0530

    added project

---
+++ src/Master/RabbitMq/Master.RabbitMq.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Master.RabbitMq.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Master/RabbitMq/Master.RabbitMq/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Master.RabbitMq
+{
+    public class Class1
+    {
+    }
+}
