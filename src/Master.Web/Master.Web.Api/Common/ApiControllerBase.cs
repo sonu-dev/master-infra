582272f31b7e993f23b446a09833c1e5febaa7d9
Sonu Thakur <sonu.concious@gmail.com>
2021-06-28 19:29:10 +0530

    Adding Master.Web

---
+++ src/Master.Web/Master.Web.Api/Common/ApiControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Master.Web.Api.Common
+{
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        public ILogger Log { get; private set; }
+        public ApiControllerBase(ILogger log)
+        {
+            Log = log;
+        }
+    }
+}
---
+++ src/Master.Web/Master.Web.Api/ViewModels/Requests/GetTodosRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Master.Web.Api.ViewModels.Requests
+{
+    public class GetTodosRequest
+    {
+        public List<int> TodoIds { get; set; }
+    }
+}
